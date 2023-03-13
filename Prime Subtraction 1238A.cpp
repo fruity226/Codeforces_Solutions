6bb3688818486d2d4e2aa450988a574541d1a0c5
ARPIT SAGAR <127712127+fruity226@users.noreply.github.com>
2023-03-14 00:33:08 +0530

    1238A

---
+++ Prime Subtraction 1238A.cpp
@@ -0,0 +1,26 @@
+/* Question link: https://codeforces.com/contest/1238/problem/A
+Tag: math, number theory, 900*
+Codeforces ID: arpit_26 */
+
+
+
+# include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    long long a,b;  
+    cin>>a>>b;
+    if(a-b==1){
+        cout<<"NO"<<endl;
+        return;
+    }
+    else cout<<"YES"<<endl;
+}
+ 
+int main(){
+    
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}

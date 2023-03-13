1c7bcbdbdbf705567f94d23030d649fcd2d729f1
fruity226 <127712127+fruity226@users.noreply.github.com>
2023-03-13 10:19:19 +0530

    Create Lame King 1804A.cpp

---
+++ Lame King 1804A.cpp
@@ -0,0 +1,27 @@
+/* Question link: https://codeforces.com/contest/1804/problem/A
+Tag: greedy, maths, 900*
+Codeforces ID: arpit_26 */
+
+# include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a,b,maxi=0; cin>>a>>b;
+    a=abs(a);
+    b=abs(b);
+    if(a==absb){
+        cout<<a+b<<endl;
+        return;
+    }
+    else{
+        maxi=max(a,b);
+        cout<<2*maxi-1<<endl;
+    }
+}
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}

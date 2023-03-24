0bde79bb414b87ef41b2cc7cf85aa924f2065b76
ARPIT SAGAR <127712127+fruity226@users.noreply.github.com>
2023-03-24 23:44:28 +0530

    199A

---
+++ A. Hexadecimal's theorem199A.cpp
@@ -0,0 +1,33 @@
+/* Question Link:https://codeforces.com/problemset/problem/199/A
+Question Tag:  brute force, constructive algorithms, implementation, number theory, *900
+Codeforces ID: arpit_26 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+#define pb push_back
+#define pob pop_back
+#define endl "\n"
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define in insert
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+typedef long long ll;
+
+void solve(){
+    long long n;  cin>>n;
+    int a=0;
+    int b=0;
+    int c=n;
+    cout<<a<<" "<<b<<" "<<c<<endl;
+}
+
+int main(){
+    int t=1;      //cin>>t;
+    while(t--){
+        solve();
+    }
+}
